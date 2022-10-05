82660d57a2afc4ecfe829066393a62de8f95108f
Rasmus Jensen <rasmusdj96@gmail.com>
2022-10-05 07:46:33 +0200

    123

--- js/fetchNews.js
+++ js/fetchNews.js
@@ -1,43 +1,44 @@
+//Opretter et nyt array for at skubbe mit data ind i
 const apiData = [];
 
 const fetchNews = () => {
-
-const apiEndpoint = "https://newsapi.org/v2/everything?q=tesla&from=2022-08-08&sortBy=publishedAt&apiKey=3cf3e094d88f40c9bd765757ad4e6bd6"
-//Endpoint er den url jeg får data far
-fetch(apiEndpoint)
-//response er serverens respons til mig - hvis det lykkes at nå mit endpoint
-.then((response) =>{
-    return response.json();
-})
-//Data er det json som jeg har returneret fra mit response
-.then((data) => {
-    console.log(data.articles.slice(0,5));
-    apiData.push(data.articles.slice(0,5));
-//Catch er hvis mit data bliver rejected
-}).catch((error) =>{
-    console.log(error);
-})
-//Hvis alt er lykkes og vi kommer forbi vores error catch så kommer vi til finally
-.finally(() =>{
-    console.log("Jeg er igennem");
-    renderContent()
-})
+	const apiEndpoint =
+		"https://newsapi.org/v2/everything?q=tesla&from=2022-08-08&sortBy=publishedAt&apiKey=3cf3e094d88f40c9bd765757ad4e6bd6";
+	//Endpoint er den url jeg får data fra
+	fetch(apiEndpoint)
+		//response er serverens respons til mig - hvis det lykkes at nå mit endpoint
+		.then((response) => {
+			return response.json();
+		})
+		//Data er det json som jeg har returneret fra mit response
+		.then((data) => {
+			console.log(data.articles.slice(0, 5));
+			apiData.push(data.articles.slice(0, 5));
+		}) //Catch er hvis mit data bliver rejected
+		.catch((error) => {
+			console.log(error);
+		})
+		//Hvis alt er lykkes og vi kommer forbi vores error catch så kommer vi til finally
+		.finally(() => {
+			renderContent();
+		});
 };
 
-const renderContent = () =>{
-    apiData[0].map((news) => createCard(news))
-}
+//Her mapper vi dataen så det er klart til at blive rendered
+const renderContent = () => {
+	apiData[0].map((news) => createCard(news));
+};
 
-const createCard = (data)=>{
-    document.getElementById("mother").innerHTML += `
+// Her render vi dataen ved at sætte det i et 'card' på selve siden
+const createCard = (data) => {
+	document.getElementById("root").innerHTML += `
 
     <div>${data.title}</div>
     <img src="${data.urlToImage}"</img>
     
     
 
-    `
-
-}
+    `;
+};
 
 fetchNews();
--- js/fetchWeather.js
+++ js/fetchWeather.js
@@ -1,29 +1,19 @@
-
-
-
-
 fetch() /**Endpoint API */
-.then()/**Response returner */
-.then() /**push data ind i et array */
-.catch() /**error handling */
-.finally() /**Hvis alt lykkedes så gør noget */
+	.then() /**Response returner */
+	.then() /**push data ind i et array */
+	.catch() /**error handling */
+	.finally(); /**Hvis alt lykkedes så gør noget */
 
-
-
-
-
-
+const arr1 = [];
 
 fetch()
-.then((response) => { 
-    return response.json
-})
-.then((data) =>{
-    arr1.push(data.items)
-})
-.catch((error) =>{
-    console.error(error);
-})
-.finally(() =>{
-
-})
+	.then((response) => {
+		return response.json;
+	})
+	.then((data) => {
+		arr1.push(data.items);
+	})
+	.catch((error) => {
+		console.error(error);
+	})
+	.finally(() => {});
